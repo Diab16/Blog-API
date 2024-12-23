ce36bbf1e1485bcd4989bfb99e9da2e51d2a1d77
Diab16 <abdohdiab166@gmail.com>
2024-12-23 22:16:12 +0200

    fix: configuration of env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOSt,
-      port: process.env.db_port as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DATABASE_NAME,
-      // entities: [],
+      entities: [],
       autoLoadEntities: true,
       synchronize: true,
     }),
-    ConfigModule.forRoot(),
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
