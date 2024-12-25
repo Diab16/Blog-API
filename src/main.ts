d3365a2c86d51b07e7088369444a07d69f9b2b0c
Diab16 <abdohdiab166@gmail.com>
2024-12-25 16:03:21 +0200

    fix: solve a bug in swagger header bearer

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     .setTitle('Blog API')
     .setDescription('simple Blog API using nestJS')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
