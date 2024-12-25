0eb8141dbc721f580ca2ddd19743461a48df02a2
Diab16 <abdohdiab166@gmail.com>
2024-12-25 16:18:25 +0200

    feat: create the relationship between users and post

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 @Entity()
 export class Post {
   @PrimaryGeneratedColumn()
@@ -9,4 +16,11 @@ export class Post {
 
   @Column()
   content: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: number;
 }
