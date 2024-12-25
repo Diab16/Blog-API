d72e72dbbbda850d7ebc4217e187e3de94ecefbb
Diab16 <abdohdiab166@gmail.com>
2024-12-25 16:11:58 +0200

    add ApiProperty

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'the title of the post',
+    required: true,
+    example: 'a new post',
+  }) // required: false if not reqird or null (title?:string)
   title: string;
+  @ApiProperty({
+    description: 'the title of the post',
+    required: true,
+    example: 'content of the post',
+  })
   content: string;
+  // for array and nested object -> @ApiProperty({type:[string],enum:Category})
 }
