93b3dba27fcf93861da8a45054e57fe9e44c3a25
Diab16 <abdohdiab166@gmail.com>
2024-12-25 16:16:32 +0200

    add ApiProperty

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -5,7 +5,7 @@ export class CreatePostDto {
     description: 'the title of the post',
     required: true,
     example: 'a new post',
-  }) // required: false if not reqird or null (title?:string)
+  })
   title: string;
   @ApiProperty({
     description: 'the title of the post',
@@ -13,5 +13,4 @@ export class CreatePostDto {
     example: 'content of the post',
   })
   content: string;
-  // for array and nested object -> @ApiProperty({type:[string],enum:Category})
 }
